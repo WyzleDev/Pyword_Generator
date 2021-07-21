25fd9c230375cb1be080bc2af9c43f86dc92fdfd
codenameWyzle <66065989+codenameWyzle@users.noreply.github.com>
2021-07-21 03:47:10 +0300

    Add files via upload

---
+++ Password_generator.py
@@ -0,0 +1,23 @@
+import os
+import random
+import PyQt5
+chars = "+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+number = int( input(" Enter number of passwords > ") )
+length = int(input(" Enter length of password > "))
+
+for x in range(number):
+    password = ''
+
+    for i in range(length):
+        password += random.choice(chars)
+
+    print(password)
+
+    file = open('password.txt', 'a')
+    file.write('\n' + password)
+    file.close()
+
+
+os.system('pause')
+
